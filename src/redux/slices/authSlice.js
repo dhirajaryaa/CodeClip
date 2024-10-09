66244cbcc32b40a41ffaa6603b0db7297ef2b88d
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-10-09 09:29:51 +0530

    feat: add signin thunk function

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,3 +1,5 @@
+import authServices from "@/services/authServices";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
@@ -7,18 +9,58 @@ const initialState = {
   isError: null,
 };
 
+export const signInWithEmailAndPassword = createAsyncThunk(
+  "auth/signin",
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const data = await authServices.emailPasswordSignIn(email, password);
+      const userData ={
+        uid:data.uid,
+        email:data.email,
+        name:data.displayName,
+        photoUrl:data.photoURL
+      } 
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
 const AuthSlice = createSlice({
-  name: auth,
+  name: "auth",
   initialState,
   reducers: {
-    setUser: (state,action) => {
-      state.user = action.payload
+    setUser: (state, action) => {
+      state.isAuth = true;
+      state.user = action.payload;
+    },
+    signOut: (state) => {
+      state.isAuth = false;
+      state.user = null;
     },
   },
-  extraReducers: {
+  extraReducers: (builder) => {
+    builder.addCase(signInWithEmailAndPassword.pending, (state) => {
+      state.isLoading = true;
+      state.isError = null;
+    });
 
+    builder.addCase(signInWithEmailAndPassword.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isError = null;
+      state.isAuth = true;
+      console.log(action.payload);
+      
+      state.user = action.payload;
+    });
+
+    builder.addCase(signInWithEmailAndPassword.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = action.payload;
+    });
   },
 });
 
-export const {setUser} = AuthSlice.actions;
+export const { setUser, signOut } = AuthSlice.actions;
 export default AuthSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/authSlice";
 
-export default store = configureStore({
+const store = configureStore({
     reducer:{
         auth:authSlice
     }
 })
+
+export default store
