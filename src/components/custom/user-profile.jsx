00afd0b733bd140f3f5e9e9216646aed99be6caa
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-10-09 11:44:55 +0530

    style: user profile menu done

--- src/components/custom/user-profile.jsx
+++ src/components/custom/user-profile.jsx
@@ -3,6 +3,16 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
 import { useSelector } from "react-redux";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOutIcon } from "lucide-react";
+import { signOut } from "@/redux/slices/authSlice";
 
 function UserProfile() {
   const { user, isAuth } = useSelector((state) => state.auth);
@@ -11,18 +21,44 @@ function UserProfile() {
   return (
     <div>
       {isAuth ? (
-        <Avatar className="w-12 duration-100 h-12 rounded-full border-[3px] border-primary p-1 hover:border-destructive-foreground">
-          <AvatarImage
-            src="https://cdn.jsdelivr.net/gh/alohe/avatars/png/memo_24.png"
-            alt="Avatar"
-          />
-          <AvatarFallback>
-            {"Avatar"
-              .split(" ")
-              .map((n) => n[0])
-              .join("")}
-          </AvatarFallback>
-        </Avatar>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar className="w-12 duration-100 h-12 rounded-full border-[3px] border-primary p-1 hover:border-destructive-foreground">
+              <AvatarImage
+                src="https://cdn.jsdelivr.net/gh/alohe/avatars/png/memo_24.png"
+                alt="Avatar"
+              />
+              <AvatarFallback>
+                {"Avatar"
+                  .split(" ")
+                  .map((n) => n[0])
+                  .join("")}
+              </AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <Button className="w-full" size="xs" variant="ghost">
+                  <User className="mr-2 h-4 w-4" />
+                  <span>Profile</span>
+                </Button>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Button
+                  onclick={signOut}
+                  className="w-full"
+                  size="xs"
+                  variant="ghost"
+                >
+                  <LogOutIcon className="mr-2 h-4 w-4" />
+                  <span>Sign Out</span>
+                </Button>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       ) : (
         <Link to={"/signin"}>
           <Button size="sm">Sign In</Button>
