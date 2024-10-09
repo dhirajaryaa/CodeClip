a160dff6773fb8a7e2ed1f92f5b7eb3b8a872c9e
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-10-09 20:42:09 +0530

    refactor: adding dashboard loading and auth checking

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,20 @@
 import { Header, ProfileSettings } from "@/components/custom";
 import React from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
+  const navigate = useNavigate();
+  const { user, isAuth, isLoading } = useSelector((state) => state.auth);
+  console.log(isAuth);
+
+  useEffect(() => {
+    if (!isAuth && !isLoading) {
+      navigate("/");
+    }
+  }, [user, isAuth, isLoading]);
+
   return (
     <>
       {/* header show  */}
