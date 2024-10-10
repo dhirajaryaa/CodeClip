fc1ed249c4f6ce9b01b2596e49c2c8f486fb3f8c
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-10-10 15:30:58 +0530

    refactor: navlinks code refactor using map

--- src/components/custom/NavLinks.jsx
+++ src/components/custom/NavLinks.jsx
@@ -1,56 +1,71 @@
-import { Folder, Home, Settings, Star, Tags } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Badge } from "../ui/badge";
 import { Logo } from ".";
+import {
+  Grid2x2Icon,
+  HeartIcon,
+  Trash2Icon,
+  FolderCodeIcon,
+  Tags,
+} from "lucide-react";
 
 function NavLinks() {
+  const navItems = [
+    {
+      name: "All Snippets",
+      icon: <Grid2x2Icon className="h-5 w-5" />,
+      link: "",
+      badge: 0,
+    },
+    {
+      name: "Favorites",
+      icon: <HeartIcon className="h-5 w-5" />,
+      link: "",
+      badge: 0,
+    },
+    {
+      name: "My Snippets",
+      icon: <FolderCodeIcon className="h-5 w-5" />,
+      link: "",
+      badge: 0,
+    },
+    {
+      name: "Trash",
+      icon: <Trash2Icon className="h-5 w-5" />,
+      link: "",
+      badge: 0,
+    },
+    {
+      name: "Tags",
+      icon: <Tags className="h-5 w-5" />,
+      link: "",
+      badge: 0,
+    },
+  ];
+
   return (
     <nav className="grid gap-2 text-md font-medium  mt-5">
-      <div
-        href="#"
-        className="flex items-center gap-2 text-lg font-semibold sm:hidden"
-      >
+      <div className="flex items-center gap-2 text-lg font-semibold sm:hidden">
         <Logo />
       </div>
-      <h3 className="text-muted-foreground/50">Quick Links</h3>
-      <Link
-        to="#"
-        className="mx-[-0.65rem] hover:bg-muted duration-100 ease-in-out flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-      >
-        <Home className="h-5 w-5" />
-        Dashboard
-      </Link>
-      <Link
-        to="#"
-        className="mx-[-0.65rem] hover:bg-muted duration-100 ease-in-out flex items-center gap-4 rounded-xl px-3 py-2  text-muted-foreground hover:text-foreground"
-      >
-        <Star className="h-5 w-5" />
-        Favorites
-        <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full text-xs">
-          582
-        </Badge>
-      </Link>
-      <Link
-        to="#"
-        className="mx-[-0.65rem] hover:bg-muted duration-100 ease-in-out flex items-center gap-4 rounded-xl px-3 py-2 text-foreground hover:text-foreground"
-      >
-        <Folder className="h-5 w-5" />
-        My Snippets
-      </Link>
-      <Link
-        to="#"
-        className="mx-[-0.65rem] hover:bg-muted duration-100 ease-in-out flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-      >
-        <Tags className="h-5 w-5" />
-        Tags
-      </Link>
-      <Link
-        to="#"
-        className="mx-[-0.65rem] hover:bg-muted duration-100 ease-in-out flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-      >
-        <Settings className="h-5 w-5" />
-        Settings
-      </Link>
+      <h3 className="text-muted-foreground/50 mt-8">Quick Links</h3>
+
+      {navItems.map(({ name, icon, link, badge }, index) => (
+        <Link
+          key={index}
+          to={link}
+          className="mx-[-0.65rem] hover:bg-muted duration-100 ease-in-out flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+        >
+          {icon}
+          {name}
+          {badge > 0 && (
+            <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full text-xs">
+              {badge}
+            </Badge>
+          )}
+        </Link>
+      ))}
+     
     </nav>
   );
 }
