00944b1b9cb574965f65727f44b3991a1dbac604
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-10-11 16:23:16 +0530

    refactor: code refined

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,6 @@
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
   Logo,
   NavLinks,
   ProfileSettings,
@@ -39,7 +32,7 @@ export const Dashboard = () => {
 
         {/* dashboard content  */}
 
-        <main className="flex flex-1 flex-col h-full max-h-[93vh] overflow-hidden w-full bg-muted p-4 md:p-6 gap-4 md:gap-8">
+        <main className="flex flex-1 flex-col h-full max-h-[93vh] overflow-hidden w-full bg-muted p-4 gap-2">
           <div className="flex items-center">
             <h1 className="text-lg font-semibold md:text-2xl">My Snippets</h1>
             <Button size="sm" className="ml-auto">
@@ -47,8 +40,15 @@ export const Dashboard = () => {
               New Snippet
             </Button>
           </div>
-          <div className="flex-1 overflow-y-auto grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 scrollbar-hide">
+          <div className="flex-1 overflow-y-auto grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 scrollbar-hide">
             {/* code snippet card  */}
+            <SnippetCard />
+            <SnippetCard />
+            <SnippetCard />
+            <SnippetCard />
+            <SnippetCard />
+            <SnippetCard />
+            <SnippetCard />
             <SnippetCard />
             <SnippetCard />
             <SnippetCard />
