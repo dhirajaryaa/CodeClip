fd63b3d9a96ac3bc623a0c2b437070d1382b62ce
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-10-11 08:18:16 +0530

    feat: makeing user add on database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import {getFirestore} from "firebase/firestore"
 // Firebase configuration using environment variables
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,5 +14,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
-export {app,auth,googleProvider}
+export {app,auth,googleProvider,db}
